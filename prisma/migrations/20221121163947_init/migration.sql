a1ef45c74bc78045b48dc8eec0bf82e4982b3d4e
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2022-11-25 14:21:28 +0100

    Connect to database

--- prisma/migrations/20221121163947_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "img" TEXT NOT NULL,
-    "name" TEXT NOT NULL
-);
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,25 +1,44 @@
+import { PrismaClient } from "@prisma/client";
 import { invalid } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
-    return {
-        question: {
-            id: 1,
-            img: "black-cat-looking-up-at-owner.jpg",
-            options: [{ id: 1, name: "Paco" }, { id: 2, name: "Pepe" }, { id: 3, name: "Macarena" }],
-        },
-        questions: [
-            {
+const prisma = new PrismaClient();
+
+function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function pick<T>(array: T[]): T {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+export const load: PageServerLoad = async () => {
+    try {
+        const user = await prisma.user.findFirstOrThrow();
+        const userNames = await prisma.user.findMany({ select: { id: true, name: true } })
+        return {
+            question: {
                 id: 1,
-                img: "black-cat-looking-up-at-owner.jpg",
-                options: [{ id: 1, name: "Paco" }, { id: 2, name: "Pepe" }, { id: 3, name: "Macarena" }],
-            },
-            {
-                id: 2,
-                img: "black-cat-looking-up-at-owner.jpg",
-                options: [{ id: 4, name: "Paco2" }, { id: 5, name: "Pepe2" }, { id: 6, name: "Macarena2" }],
-            },
-        ]
+                img: user.img,
+                options: shuffle([user, ...userNames.filter(({ id }) => id !== user.id).slice(0, 3)]),
+            }
+        };
+    } finally {
+        await prisma.$disconnect();
     }
 }
 
@@ -30,12 +49,18 @@ export const actions: Actions = {
         if (!chosen) {
             return invalid(400, { chosen, missing: true });
         }
-        await new Promise(r => setTimeout(r, 1000));
+        // await new Promise(r => setTimeout(r, 1000));
+
+        const allUsers = await prisma.user.findMany();
+        const someUsers = shuffle(allUsers).slice(0, 4);
+
+        const next = pick(someUsers);
+
         return {
             success: chosen && +chosen.valueOf() === 1, next: {
-                id: 2,
-                img: "sam-chang-5-ckjYvTZQw-unsplash.jpg",
-                options: [{ id: 4, name: "Paco2" }, { id: 5, name: "Pepe2" }, { id: 6, name: "Macarena2" }],
+                id: next.id,
+                img: next.img,
+                options: someUsers
             },
         };
     }
